98f6d98f2d5eb298570acfaf8c43e280cc3579c8
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-05-06 14:21:22 +0400

    working with check_contracts.html

--- views/check_contracts.py
+++ views/check_contracts.py
@@ -1,24 +1,33 @@
 from flask import Blueprint, render_template, request, redirect, jsonify, flash, url_for
-from sqlalchemy.orm import selectinload, join
-from sqlalchemy import select
+from forms.contract_search import SearchContract
 from database.models import *
 from database.db_init import db
-
+from database.validators import SearchEngine
 check_contracts_bp = Blueprint('all_contracts', __name__)
 
 
 @check_contracts_bp.route('/all_contracts', methods=['GET'])
 def get_all_contracts():
+    form = SearchContract()
+    search_query = request.args.get("search")
+    if search_query:
+        search_engine = SearchEngine(search_query, db.session)
+        search_results = search_engine.search_voen()
+        print(search_results)
+        return render_template("check_contracts.html",
+                               all_contracts=False,
+                               new_contract=True,
+                               dashboard=True,
+                               companies=search_results,
+                               form=form
+                               )
+
     stmt = db.session.query(Companies).join(Contract.company).distinct()
-    print(stmt)
     results = db.session.execute(stmt)
     companies = results.scalars().all()
-
-    # stmt = select(Companies).join(VOEN, Companies.id == VOEN.company_id).join(Contract, VOEN.id == Contract.voen_id)
-    # results = db.session.execute(stmt)
-    # companies = results.scalars().all()
     return render_template("check_contracts.html",
                            all_contracts=False,
                            new_contract=True,
                            dashboard=True,
-                           companies=companies)
+                           companies=companies,
+                           form=form)
