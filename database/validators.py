c6d7985cf99660a51267a3a077f93faa849181f6
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-05-16 17:41:02 +0400

    change the logic of my backend part and work with db

--- database/validators.py
+++ database/validators.py
@@ -1,3 +1,4 @@
+from typing import Any
 from flask_sqlalchemy.session import Session
 from sqlalchemy import or_
 from sqlalchemy.exc import NoResultFound, IntegrityError, ArgumentError
@@ -42,7 +43,7 @@ class ContractManager:
 
 
 class SearchEngine:
-    def __init__(self, search: str, db_session: Session):
+    def __init__(self, db_session: Session, search: str = None):
         self.search = search
         self.db_session = db_session
 
@@ -52,8 +53,8 @@ class SearchEngine:
             Companies.voen.ilike(f"%{self.search}%"),
             Contract.contract_number.ilike(f"%{self.search}%")
         )).all()
-        print(len(results))
         filtered_results = self.filter_results(results)
+        print(filtered_results)
         return filtered_results
 
     def search_voen(self) -> list:
@@ -64,16 +65,39 @@ class SearchEngine:
     def filter_results(self, results: list) -> list:
         filtered_results = []
         for result in results:
-            print(result.company_name, result.voen)
+            # print(result.company_name, result.voen)
             if (result.voen.find(self.search) != -1) or (result.company_name.find(self.search) != -1):
                 filtered_results.append(result)
             else:
                 for contract in result.contracts:
                     filtered_results.append(contract) if contract.contract_number.find(self.search) != -1 else None
-        print(filtered_results)
+        # print(filtered_results)
         return filtered_results
+
+    @staticmethod
+    def flat_results(results: list) -> list:
+        flat_results = []
+        for result in results:
+            if isinstance(result, Companies):
+                for contract in result.contracts:
+                    flat_results.append(contract)
+            else:
+                flat_results.append(result)
+        return flat_results
 
     def search_company(self) -> Contract:
         result = self.db_session.query(Contract).join(Companies).filter(Contract.id == self.search).first()
-
         return result
+
+    def get_all_results(self, db, page: int, per_page: int) -> dict[str, int | Any]:
+        # stmt = self.db_session.query(Companies).join(Contract.company).distinct()
+        stmt = self.db_session.query(Contract).join(Contract.company).distinct()
+        # results = self.db_session.execute(stmt)
+        # companies = results.scalars().all()
+        # total_contracts = sum(len(company.contracts) for company in companies)
+        # for company in companies:
+        total_contracts = stmt.count()
+        limited_results = db.paginate(stmt, page=page, per_page=per_page, error_out=False).items
+        return {"results_per_page": limited_results,
+                "total_contracts": total_contracts
+                }
--- views/check_contracts.py
+++ views/check_contracts.py
@@ -1,38 +1,47 @@
 import flask_wtf
 from flask import Blueprint, render_template, request
 from forms.contract_search import SearchContract
-from database.models import *
 from database.db_init import db
 from database.validators import SearchEngine
+from configuration import POSTS_PER_PAGE
 
 
-def handle_search(search_query: str, form: flask_wtf.Form) -> render_template:
-    search_engine = SearchEngine(search_query, db.session)
+def handle_search(search_query: str, form: flask_wtf.Form, page: int) -> render_template:
+    search_engine = SearchEngine(db.session, search_query)
     search_results = search_engine.search_query()
-    total_contracts = 0
-    for item in search_results:
-        if isinstance(item, Companies):
-            total_contracts += len(item.contracts)
-        elif isinstance(item, Contract):
-            total_contracts += 1
-
+    flattened_results = search_engine.flat_results(search_results)
+    # total_contracts = search_results["total_results"]
+    # companies = search_results["results"]
+    # total_pages = (total_contracts // POSTS_PER_PAGE) + (1 if total_contracts % POSTS_PER_PAGE != 0 else 0)
+    total_contracts = len(flattened_results)
+    print(total_contracts)
+    total_pages = (total_contracts // POSTS_PER_PAGE) + (1 if total_contracts % POSTS_PER_PAGE != 0 else 0)
+    start = (page - 1) * POSTS_PER_PAGE
+    end = start + POSTS_PER_PAGE
+    paginated_results = [item for item in flattened_results][start:end]
     return render_template("check_contracts.html",
                            amount_of_companies=total_contracts,
-                           companies=search_results,
-                           form=form
+                           companies=paginated_results,
+                           form=form,
+                           page=page,
+                           total_pages=total_pages,
+                           action="search",
+                           search_query=search_query
                            )
 
 
-def handle_all_contracts(form: flask_wtf.Form) -> render_template:
-    stmt = db.session.query(Companies).join(Contract.company).distinct()
-    results = db.session.execute(stmt)
-    companies = results.scalars().all()
-    total_contracts = sum(len(company.contracts) for company in companies)
-    print("all")
+def handle_all_contracts(form: flask_wtf.Form, page: int) -> render_template:
+    search_engine = SearchEngine(db.session)
+    search_results = search_engine.get_all_results(db, page, POSTS_PER_PAGE)
+    total_contracts = search_results["total_contracts"]
+    companies = search_results["results_per_page"]
+    total_pages = (total_contracts // POSTS_PER_PAGE) + (1 if total_contracts % POSTS_PER_PAGE != 0 else 0)
     return render_template("check_contracts.html",
                            amount_of_companies=total_contracts,
                            companies=companies,
                            form=form,
+                           page=page,
+                           total_pages=total_pages,
                            action="all"
                            )
 
@@ -43,20 +52,21 @@ check_contracts_bp = Blueprint('all_contracts', __name__)
 @check_contracts_bp.route('/all_contracts', methods=['GET'])
 def get_all_contracts():
     form = SearchContract()
-    action = request.args.get("action", None)
+    action = request.args.get("action", "all")
+    page = request.args.get("page", 1, type=int)
 
     match action:
         case "search":
             search_query = request.args.get("search", None)
-            return handle_search(search_query, form)
+            return handle_search(search_query, form, page)
         case "all":
-            return handle_all_contracts(form)
-    return handle_all_contracts(form)
+            return handle_all_contracts(form, page)
+    return handle_all_contracts(form, page)
 
 
 @check_contracts_bp.route('/contract/<int:contract_id>', methods=['GET'])
 def get_contract(contract_id):
-    search_engine = SearchEngine(contract_id, db.session)
+    search_engine = SearchEngine(db.session, contract_id)
     search_result = search_engine.search_company()
     print(search_result)
     return render_template("check_contract.html", search_result=search_result)
