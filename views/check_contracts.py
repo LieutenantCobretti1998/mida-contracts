6191b11fc16b91c59b4248739fecad2b2893294b
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-05-21 17:09:05 +0400

    some major changes for url appearing in better for better user experience

--- views/check_contracts.py
+++ views/check_contracts.py
@@ -1,5 +1,5 @@
 import flask_wtf
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session, redirect, url_for
 from forms.contract_search import SearchContract
 from database.db_init import db
 from database.validators import SearchEngine
@@ -11,15 +11,19 @@ def handle_search(search_query: str, form: flask_wtf.Form, page: int, filters: s
     search_results = search_engine.search_query(page, POSTS_PER_PAGE, filters, order)
     total_contracts = search_results["total_contracts"]
     total_results = search_results["results_per_page"]
-    print(len(total_results))
     total_pages = (total_contracts // POSTS_PER_PAGE) + (1 if total_contracts % POSTS_PER_PAGE != 0 else 0)
+
+    if page > total_pages:
+        return redirect(
+            url_for('all_contracts.get_all_contracts', action="search", page=total_pages, filters=filters, orders=order,
+                    search=search_query))
     return render_template("check_contracts.html",
                            amount_of_companies=total_contracts,
                            companies=total_results,
                            form=form,
                            page=page,
                            total_pages=total_pages,
-                           action="search",
+                           action=session["action"],
                            search_query=search_query,
                            filters=filters,
                            order=order
@@ -31,15 +35,16 @@ def handle_all_contracts(form: flask_wtf.Form, page: int) -> render_template:
     search_results = search_engine.get_all_results(db, page, POSTS_PER_PAGE)
     total_contracts = search_results["total_contracts"]
     companies = search_results["results_per_page"]
-    print(len(companies))
     total_pages = (total_contracts // POSTS_PER_PAGE) + (1 if total_contracts % POSTS_PER_PAGE != 0 else 0)
+    if page > total_pages:
+        return redirect(url_for('all_contracts.get_all_contracts', action="all", page=total_pages))
     return render_template("check_contracts.html",
                            amount_of_companies=total_contracts,
                            companies=companies,
                            form=form,
                            page=page,
                            total_pages=total_pages,
-                           action="all"
+                           action=session["action"],
                            )
 
 
@@ -49,15 +54,21 @@ check_contracts_bp = Blueprint('all_contracts', __name__)
 @check_contracts_bp.route('/all_contracts', methods=['GET'])
 def get_all_contracts():
     form = SearchContract()
-    action = request.args.get("action", "all")
+    action = request.args.get("action")
+    session["action"] = action
     page = request.args.get("page", 1, type=int)
-
+    session["page"] = page
     match action:
         case "search":
-            filters = request.args.get("filters", None)
-            order = request.args.get("order", None)
-            search_query = request.args.get("search", None)
-            return handle_search(search_query, form, page, filters, order)
+            print(request.args.get("filters"))
+            filters = request.args.get("filters", "")
+            orders = request.args.get("orders", "")
+            session["filters"] = filters
+            session["order"] = orders
+            search_query = request.args.get("search", "").strip()
+            session["search_query"] = search_query
+            print(type(search_query))
+            return handle_search(search_query, form, page, filters, orders)
         case "all":
             return handle_all_contracts(form, page)
     return handle_all_contracts(form, page)
