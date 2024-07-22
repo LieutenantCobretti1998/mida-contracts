da405eb2ef03e73d85aed32f96b421d9524bdadd
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-07-22 10:49:24 +0400

    working on redesign

--- views/check_contracts.py
+++ views/check_contracts.py
@@ -39,7 +39,6 @@ def handle_search(search_query: str, form: flask_wtf.Form, page: int = None, fil
 def handle_all_contracts(form: flask_wtf.Form, page: int = None) -> render_template:
     search_engine = SearchEngine(db.session)
     search_results = search_engine.get_all_results(db, page, POSTS_PER_PAGE)
-    # search_engine.get_all_results_api(POSTS_PER_PAGE)
     total_contracts = search_results["total_contracts"]
     companies = search_results["results_per_page"]
     total_pages = (total_contracts // POSTS_PER_PAGE) + (1 if total_contracts % POSTS_PER_PAGE != 0 else 0)
@@ -61,33 +60,16 @@ check_contracts_bp = Blueprint('all_contracts', __name__)
 
 @check_contracts_bp.route('/all_contracts', methods=['GET'])
 def get_all_contracts():
-    # form = SearchContract()
-    # action = request.args.get("action")
-    # session["contract_action"] = action
-    # page = request.args.get("page", 1, type=int)
-    # session["contract_page"] = page
-    # match action:
-    #     case "search":
-    #         filters = request.args.get("filters", "")
-    #         orders = request.args.get("orders", "")
-    #         session["contract_filters"] = filters
-    #         session["contract_order"] = orders
-    #         search_query = request.args.get("search", "").strip()
-    #         session["contract_search_query"] = search_query
-    #         return handle_search(search_query, form, page, filters, orders)
-    #     case "all":
-    #         session["contract_filters"] = ""
-    #         session["contract_order"] = ""
-    #         session["contract_search_query"] = ""
-    #         return handle_all_contracts(form, page)
-    # return handle_all_contracts(form, page)
     return render_template("check_contracts.html")
 
 
 @check_contracts_bp.route('/contract/<int:contract_id>', methods=['GET'])
 def get_contract(contract_id):
     form = EditContractForm()
+    search_engine = SearchEngine(db.session, contract_id)
+    search_result = search_engine.search_company_with_contract()
     return render_template("check_contract.html",
+                           search_result=search_result,
                            contract_id=contract_id,
                            form=form)
 
