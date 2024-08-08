74570d1174c7f86de3074016962cc9297383fe19
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-08-08 18:04:47 +0400

    add new listener for edit page and upgrade the old one. Need to think how combine them!

--- static/js/edit-listener-act.js
+++ static/js/edit-listener-act.js
@@ -1,14 +1,15 @@
-"use strict"
+"use strict";
 import pdfReader from "./file_loader.js";
-function updateSaveButtonState(input_fields=null, save_btn=null, adv_yes=null, adv_no=null) {
-    const pdf_file_input = document.getElementById("pdf_file");
+
+
+function updateSaveButtonState(input_fields=null, save_btn=null) {
+    const pdf_file_input = document.getElementById("pdf_file_act");
     const file = pdf_file_input.files[0];
 
     const all_fields_empty = input_fields.every(input => input.value.trim() === "");
-    const no_radio_checked = !adv_yes.checked && !adv_no.checked;
     const no_file_selected = !file;
 
-    const should_disable_button = all_fields_empty && no_radio_checked && no_file_selected;
+    const should_disable_button = all_fields_empty  && no_file_selected;
 
     save_btn.disabled = should_disable_button;
     save_btn.style.pointerEvents = should_disable_button ? "none" : "auto";
@@ -22,24 +23,17 @@ function listenEditFields() {
     save_button_element.style.pointerEvents = "none";
     save_button_element.style.cursor = "default";
     save_button_element.style.backgroundColor = "#EEEDEB";
-    const input_fields = Array.from(document.querySelectorAll("table input")).slice(0, -4);
-    const adv_payer_yes = document.getElementById("is_adv_payer-0");
-    const adv_payer_no = document.getElementById("is_adv_payer-1");
+    const input_fields = Array.from(document.querySelectorAll("table input")).slice(0, -2);
+
     const update_state = () => updateSaveButtonState(
         input_fields,
-        save_button_element,
-        adv_payer_yes,
-        adv_payer_no,
-    );
-
-    adv_payer_yes.addEventListener("change", update_state);
-    adv_payer_no.addEventListener("change", update_state);
+        save_button_element
+    )
 
     input_fields.forEach(el => {
         el.addEventListener("input", update_state);
     });
-
-    document.getElementById("pdf_file").addEventListener("change", (event) => {
+    document.getElementById("pdf_file_act").addEventListener("change", (event) => {
     const file = event.target.files[0];
     const reader_pdf = pdfReader(file, null, true, update_state)
     if (!file) {
