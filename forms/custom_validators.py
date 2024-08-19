49fe84d8cef4b1707c8ad2bc8ef1851fdc6c8b6b
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-08-19 18:22:03 +0400

    add new validators for checking the amount and remained amount for the contracts

--- forms/custom_validators.py
+++ forms/custom_validators.py
@@ -8,7 +8,7 @@ empty_space_message = "Please enter a value instead of a pure space"
 voen_number_enough_error = "Please enter voen's number with 10 characters."
 swift_number_enough_error = "Please enter the swift number with characters between 8 and 11 symbols."
 voen_only_number_error = "VOEN should contain only numbers"
-amount_error_message = "The max sum is 1.000.000.000 "
+amount_error_message = "The max sum should be greater than 1 and until 1 million "
 pdf_files_only = "Only pdf files are allowed to download"
 check_adv = "Please choose yes or no, please"
 
@@ -57,3 +57,46 @@ def add_contract_pdf(directory: str, pdf_filename: str, company_name: str = None
         file_path = os.path.join(directory_folder, unique_filename)
 
     return file_path
+
+
+def check_amount(act_amount: float, total_amount: float) -> float:
+    """
+    :param act_amount:
+    :param total_amount:
+    :return: float
+    Simple validation logic to be sure that act's amount is smaller:3
+    """
+    remained_amount = total_amount - act_amount
+    if remained_amount < 0:
+        raise ValueError
+    return remained_amount
+
+
+def check_act_amount_difference(new_act_amount: float, old_act_amount: float) -> float:
+    """
+    :param new_act_amount:
+    :param old_act_amount:
+    :return: float
+    Check the act difference between the new amount and the old amount.
+    """
+    difference = new_act_amount - old_act_amount
+    return difference
+
+
+def calculate_amount(original_amount: float, new_amount: float, old_remained_amount: float = 0) -> float:
+    """
+    :param original_amount:
+    :param new_amount:
+    :param old_remained_amount:
+    :return:
+    If the user edit the amount then calculate and add new amount to remained one
+    """
+    new_remained_amount = None
+    if original_amount > new_amount:
+        new_remained_amount = (new_amount - original_amount) + old_remained_amount
+    elif original_amount < new_amount:
+        new_remained_amount = (new_amount - original_amount) + old_remained_amount
+    if new_remained_amount < 0:
+        raise ValueError
+    return new_remained_amount
+
