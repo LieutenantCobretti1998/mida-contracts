4617ffd0cd6ac1cfc60f2a8d3abb7c5aa36dafd4
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-08-21 17:54:58 +0400

    edit addition logic

--- views/check_additions.py
+++ views/check_additions.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, render_template, redirect, flash, url_for, abort, send_file, jsonify
 from werkzeug.utils import secure_filename
 from sqlalchemy.exc import NoResultFound, DBAPIError, OperationalError
-from database.validators import ActsSearchEngine, EditAct, AdditionManager, AdditionSearchEngine
+from database.validators import AdditionManager, AdditionSearchEngine, EditAddition
 from database.db_init import db
 from forms.custom_validators import check_amount, check_act_amount_difference, calculate_amount
-from forms.edit_act_form import EditActForm
 from forms.edit_addition import EditAdditionForm
 from forms.filters import *
 
@@ -42,47 +41,59 @@ def edit_addition(addition_id):
 @check_additions_bp.route('/update_act/<int:addition_id>', methods=['POST'])
 def update_addition(addition_id):
     search_engine = AdditionSearchEngine(db.session, addition_id)
-    edit_engine = EditAct(db.session, addition_id)
-    original_data = search_engine.search_act()
-    form = EditActForm()
+    edit_engine = EditAddition(db.session, addition_id)
+    original_data = search_engine.search_addition()
+    form = EditAdditionForm()
 
     if form.validate_on_submit():
-        if form.act_amount.data:
+        if form.addition_amount.data and form.contract_id == original_data.contract_id:
             try:
-                difference = check_act_amount_difference(form.act_amount.data, original_data.amount)
-                check_amount(form.act_amount.data, original_data.amount)
-                search_engine.decrease_or_increase_difference_amount(form.contract_id.data, difference)
+                difference = original_data.amount - form.addition_amount.data
+                match difference > 0:
+                    case True:
+                        remained_amount = original_data.amount - difference
+                        new_total_amount = original_data.contract.amount - difference
+                        print(new_total_amount, remained_amount)
+                        edit_engine.change_old_contract(remained_amount, new_total_amount)
+                        return
+                    case False:
+                        remained_amount = original_data.amount - difference * -1
+                        new_total_amount = original_data.contract.amount - difference * -1
+                        print(new_total_amount, remained_amount)
+                        edit_engine.change_old_contract(remained_amount, new_total_amount)
+                        return
+
             except NoResultFound:
                 abort(404)
             except (DBAPIError, OperationalError):
                 flash("Something went wrong in the database", "error")
-                return render_template('edit_act.html', form=form, act_id=addition_id, search_result=original_data)
+                return render_template('edit_act.html', form=form, addition_id=addition_id, search_result=original_data)
             except ValueError:
                 flash("Act's amount is bigger than the total contract's amount. Please check act amount field", "warning")
-                return render_template('edit_act.html', form=form, act_id=addition_id, search_result=original_data)
+                return render_template('edit_act.html', form=form, addition_id=addition_id, search_result=original_data)
         file = form.pdf_file_act.data
         filename = ""
         if file:
             filename = secure_filename(make_unique(f"{file.filename}"))
         data_dict = dict(
-            act_number=filter_act_number(form.act_number.data) if form.act_number.data else original_data.act_number,
+            addition_number=filter_act_number(form.addition_number.data) if form.addition_number.data else original_data.addition_number,
             pdf_file_path=filename if file else None,
-            date=form.act_date.data if form.act_date.data else original_data.date,
-            amount=form.act_amount.data if form.act_amount.data else original_data.amount,
+            date=form.addition_date.data if form.addition_date.data else original_data.date,
+            amount=form.addition_amount.data if form.addition_amount.data else original_data.amount,
             contract_id=form.contract_id.data if form.contract_id.data else original_data.contract_id,
         )
         success, message = edit_engine.update_data(data_dict, form.pdf_file_act.data)
         if success:
             db.session.commit()
             flash(message, "success")
-            return redirect(url_for('all_acts.get_act', act_id=addition_id))
+            return redirect(url_for('all_additions.get_addition', addition_id=addition_id))
         else:
             db.session.rollback()
             flash(message, "warning")
-            return redirect(url_for('all_acts.edit_act', act_id=addition_id))
+            return render_template('edit_addition.html', form=form, addition_id=addition_id, search_result=original_data)
     else:
         flash("Validation Error. Please check all fields", "error")
-        return render_template('edit_act.html', form=form, act_id=addition_id, search_result=original_data)
+        return render_template('edit_addition.html', form=form, addition_id=addition_id, search_result=original_data)
 
 
 @check_additions_bp.route('/preview_pdf/<int:addition_id>', methods=['GET'])
