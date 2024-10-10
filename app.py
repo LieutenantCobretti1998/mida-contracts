5859be6d5a3f9ff1181fba4b55978c5f0eae9ec6
Jamil Aliyev <djamil5099@gmail.com>
2024-10-10 11:34:20 +0400

    change ip for local virtual machine

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from init import create_app
 app = create_app()
 
 @app.cli.command("run_waitress_prod")
-@click.option("--host", default="10.100.176.25", help="The host to listen on.")
+@click.option("--host", default="10.100.168.50", help="The host to listen on.")
 @click.option("--port", default=8080, help="The port to listen on.")
 @click.option("--threads", default=32, help="The threads to listen on.")
 def run_waitress_prod(host, port, threads):
     serve(app, host=host, port=port, threads=threads)
 
 
-# if __name__ == '__main__':
+# if __name__ == '__main__':I I am deploying
 #     csrf.init_app(app)
 #     serve(app, host='0.0.0.0', port=8080, threads=4)
